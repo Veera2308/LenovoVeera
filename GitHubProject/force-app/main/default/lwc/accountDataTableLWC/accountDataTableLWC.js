b7fcb99ce7a02eee1ff3e023785fbb7592786913
Veerasekaran <MY_NAME@example.com>
2024-06-24 18:45:06 +0530

    commit aura

--- GitHubProject/force-app/main/default/lwc/accountDataTableLWC/accountDataTableLWC.js
+++ GitHubProject/force-app/main/default/lwc/accountDataTableLWC/accountDataTableLWC.js
@@ -1,6 +1,9 @@
 import { LightningElement , track, wire} from 'lwc';
 import getAccount from '@salesforce/apex/AccountController.getAccountList';
 import { updateRecord } from 'lightning/uiRecordApi';
+//import { getPicklistValues, getObjectInfo } from 'lightning/uiObjectInfoApi';
+//import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+//import  PICKLIST_FIELD from '@salesforce/schema/Account.Rating';
 import { refreshApex } from '@salesforce/apex';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
@@ -8,7 +11,7 @@ export default class AccountDataTable extends LightningElement {
 
 //onrowaction
 
-
+@track accounts;
 @track accList;
 @track error;
 @track filteredData =[];
@@ -20,14 +23,22 @@ selectedRowCount;
 @track isSpinner = false;
 saveDraftValues = [];
 draftValues = [];
-totalRecords;
+@track pageSize=10;
+totalPages; 
+pageNumber = 1; 
+recordsToDisplay = [];
+@track pickListOptions;
+
 get options() {
         return [
             { label: '10', value: '10' },
             { label: '25', value: '25' },
-            { label: '50', value: '50' }
+            { label: '50', value: '50' },
+            { label: '100', value: '100'}
         ];
     }
+
+
 
 @track accountColumn = [{
                 label: 'Account name',
@@ -51,15 +62,30 @@ get options() {
                 type: 'phone',
                 sortable: true,
                 editable: true
-        },{
-                label: 'Rating',
-                fieldName: 'Rating',
-                type: 'text',
-                sortable: true,
         }
+        /*{
+                label: 'Rating', fieldName: 'Rating', type: 'picklistColumn', sortable: true, editable: true,
+                typeAttribute: {
+                        placeholder: 'Choose Rating', options: { fieldName: 'pickListOptions' }, 
+                        value: { fieldName: 'Rating' }, 
+                        context: { fieldName: 'Id' } 
+                }
+        }*/
 ];
+/*
+@wire(getObjectInfo, { objectApiName: ACCOUNT_OBJECT })
+objectInfo;
 
+@wire(getPicklistValues, {recordTypeId: "$objectInfo.data.defaultRecordTypeId", fieldApiName: PICKLIST_FIELD })
+wirePickList({ error, data }) {
+        if (data) {
+            this.pickListOptions = data.values;
+        } else if (error) {
+            console.log(error);
+        }
+    }   
 
+*/
 connectedCallback() {
         this.isSpinner = true;
 }
@@ -69,32 +95,38 @@ wiredAccounts({error,data}) {
         this.isSpinner = false;
         if (data) {
         this.accList = data;
-        console.log(this.accList.length)
         this.totalRecords = this.accList.length;
+        /*
+        this.accounts = JSON.parse(JSON.stringify(result.data));
+        this.accounts.forEach(ele=>{
+                ele.pickListOptions=this.pickListOptions;
+        })
+        */
         this.filterData(); 
-        console.log('d')     
-        console.log(this.accList);   
+        this.paginationHelper();
+        
 } else if (error) {
         this.error = error;
-        console.log(JSON.stringify(this.error));
 }
 }
 
 searchChange(event){
         this.searchData = event.target.value;
-        this.isSpinner = true;
-        this.debounceFilter();
+        console.log(this.searchData.length)
+        
+        this.searchData.length === 0 ? this.timeOutClear() : this.debounceFilter() ;
 }
 
 debounceFilter(){
         clearTimeout(this.delayTimeOut);       
         this.delayTimeOut = setTimeout(() => { 
                 this.filterData();
-        },300)  
+        },300)   
+        this.isSpinner = true;
+
 }
 
 filterData(){
-      
         this.filteredData = this.accList.filter(record =>
                 Object.values(record).some(value =>
                 String(value).toLowerCase().includes(this.searchData.toLowerCase())
@@ -108,8 +140,8 @@ handleRowAction(event){
                 const selectedRows = event.target.selectedRows;
                 var selectedRecords = this.template.querySelector("lightning-datatable").getSelectedRows(); 
                 this.selectedRowCount = selectedRows.length;
-                console.log( this.selectedRowCount)
-        for(let i=0; i<selectedRows.length; i++){
+
+                for(let i=0; i<selectedRows.length; i++){
                 this.selectedRecordId.push(selectedRecords[i].Id);      
         }
 }
@@ -137,6 +169,7 @@ handleSave(event) {
         Promise.all(promises).then(() => {
             this.ShowToast('Success', 'Records Updated Successfully!', 'success', 'dismissable');
             this.saveDraftValues = [];
+            this.refresh();
         }).catch(error => {
             console.log("Error:", error);
             this.ShowToast('Error', 'An Error Occurred!!', 'error', 'dismissable');
@@ -145,7 +178,7 @@ handleSave(event) {
             });
     }
     
-
+/*
     async handleSave2(event) {
     console.log( 
       'event.detail.draftValues' + JSON.stringify(event.detail.draftValues)
@@ -183,9 +216,70 @@ handleSave(event) {
       )
     }
   } 
+*/
 
-  
-      
+
+
+
+
+handleRecordsPerPage(event) {
+        this.pageSize = event.target.value;
+        this.paginationHelper();
+}
+    
+get disableFirst(){
+        return this.pageNumber == 1;
+}
+   
+get disableLast(){
+        return this.pageNumber == this.totalPages;
+}
+
+
+
+    previousPage() {
+        this.pageNumber = this.pageNumber - 1;
+        this.paginationHelper();
+    }
+    nextPage() {
+        this.pageNumber = this.pageNumber + 1;
+        this.paginationHelper();
+    }
+    firstPage() {
+        this.pageNumber = 1;
+        this.paginationHelper();
+    }
+    lastPage() {
+        this.pageNumber = this.totalPages;
+        this.paginationHelper();
+    }
+
+ timeOutClear(){
+        clearTimeout(this.delayTimeOut); 
+        this.isSpinner = false;
+        this.paginationHelper();
+ }   
+paginationHelper(){
+
+        this.filteredData = [];
+        let recordsDisplay = [];
+
+        this.totalPages = Math.ceil(this.totalRecords / this.pageSize);
+        console.log(this.totalPages)
+        if (this.pageNumber <= 1) {
+            this.pageNumber = 1;
+        } else if (this.pageNumber >= this.totalPages) {
+            this.pageNumber = this.totalPages;
+        }
+
+        for (let i = (this.pageNumber - 1) * this.pageSize; i < this.pageNumber * this.pageSize; i++) {
+            if (i === this.totalRecords) {
+                break;
+            }
+            recordsDisplay.push(this.accList[i]);
+        }
+        this.filteredData = [...recordsDisplay];
+}
 
 ShowToast(title, message, variant, mode){
         const evt = new ShowToastEvent({
@@ -201,4 +295,7 @@ ShowToast(title, message, variant, mode){
     async refresh() {
         await refreshApex(this.filteredData);
     }
+
+
+
 }
